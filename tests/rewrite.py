4cc84277d6dd116a0ee57bf7a80e9632c511374c
Michael Axiak <mike@axiak.net>
2010-06-26 02:13:50 -0400

    Added some comments

--- tests/rewrite.py
+++ tests/rewrite.py
@@ -4,6 +4,7 @@ from typedispatch import *
 # automagically using regex.
 @dispatch(regexmulti(r'(\d{3})\D{0,2}(\d{3})\D?(\d{4})'))
 def phone_numbers(phone_numbers):
+    # We found some phone numbers!
     for match in phone_numbers:
         print '-'.join(match.groups())
 
@@ -13,6 +14,11 @@ def phone_numbers(text):
     print "Could not find a phone number :-("
 
 
+
+
+
+
+# This just looks to see if the regex is inside the list.
 @dispatch([regexsingle(r'(ab+)'), str])
 def rewrite_in_list(arg):
     match, title = arg
